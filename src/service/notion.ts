cca7e1cb15d5ebfc39d493ae62aaf97894f95e84
Timothy <Rorie.TimothyJr@gmail.com>
2022-08-15 18:34:42 -0400

    Modify Notion API call logic
    Store query result locally and step through array.
    Catch ID and Properties for each page in Notion DB.
    Forced @ts-nocheck for functionality test.

--- src/service/notion.ts
+++ src/service/notion.ts
@@ -1,15 +1,105 @@
+// @ts-nocheck
+
 import { Client } from "@notionhq/client";
 import dotenv from "dotenv";
 dotenv.config();
 
-const databaseID: string = process.env.NOTION_DATABASE_ID as string;
+const notionClient: Client = new Client({ auth: process.env.NOTION_TOKEN });
+const databaseId: string = process.env.NOTION_DATABASE_ID as string;
 
-export const notion = async () => {
-  const notionAPI = new Client({
-    auth: process.env.NOTION_TOKEN,
-  });
+interface PromiseComplete {
+  notion():
+    Promise<Array<{ pageId: string; task: string; description: string; }>>;
+}
 
-  const { results } = await notionAPI.databases.query({
-    database_id: databaseID,
-  });
+//Gets tasks from the database.
+export const notion = async (): PromiseComplete => {
+  const pages = [];
+  let cursor = undefined;
+
+  while (true) {
+    const { results, next_cursor } = await notionClient.databases.query({
+      database_id: databaseId,
+      start_cursor: cursor,
+    });
+
+    pages.push(...results);
+
+    if (!next_cursor) {
+      break;
+    }
+
+    cursor = next_cursor;
+  }
+  
+  console.log(`${pages.length} pages successfully fetched.`);
+
+  const tasks = [];
+  for (const page of pages) {
+    const pageId = page.id;
+
+    const descriptionPropertyId = page.properties["Desc"].id;
+    const descriptionPropertyItem = await getPropertyValue({
+      pageId,
+      propertyId: descriptionPropertyId,
+    });
+    const description = descriptionPropertyItem.select
+      ? descriptionPropertyItem.select.name
+      : "No Description";
+
+    const titlePropertyId = page.properties["Task"].id
+    const titlePropertyItems = await getPropertyValue({
+      pageId,
+      propertyId: titlePropertyId,
+    });
+    const title = titlePropertyItems
+      .map(propertyItem => propertyItem.title.plain_text != ''
+        ? "propertyItem.title.plain_text"
+        : "No Title")
+      .join("");
+
+    tasks.push({ pageId, title, description, });
+  }
+
+  return tasks;
+}
+
+// If property is paginated, returns an array of property items.
+// Otherwise, it will return a single property item.
+
+interface PropertyValue {
+  pageId: string;
+  propertyId: string;
+}
+
+interface PromisePayload {
+  getPropertyValue({ pageId, propertyId }: PropertyValue):
+    Promise<PropertyItemObject | Array<PropertyItemObject>>;
+}
+
+const getPropertyValue = async ({ pageId, propertyId }: PropertyValue): PromisePayload => {
+  const propertyItem = await notionClient.pages.properties.retrieve({
+    page_id: pageId,
+    property_id: propertyId,
+  })
+  if (propertyItem.object === "property_item") {
+    return propertyItem;
+  }
+
+  // Property is paginated.
+  let nextCursor = propertyItem.next_cursor;
+  const results = propertyItem.results;
+
+  while (nextCursor !== null) {
+    const propertyItem = await notionClient.pages.properties.retrieve({
+      page_id: pageId,
+      property_id: propertyId,
+      start_cursor: nextCursor,
+    })
+
+    nextCursor = propertyItem.next_cursor;
+    results.push(...propertyItem.results);
+  }
+
+  return results;
 }
