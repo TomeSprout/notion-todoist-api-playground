c249ab0a3bf2ae725a4d35bed3a52ae97d2c3de9
Timothy <Rorie.TimothyJr@gmail.com>
2022-08-15 18:30:17 -0400

    Interface for Notion Query Payload

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import { notion } from "./service/notion";
 import { todoist } from "./service/todoist";
 
+interface NotionTasksPayload {
+  [index: number]: {
+    object: string;
+    id: string;
+    "created_time": string;
+    "last_edited_time": string;
+    "created_by": { object: string; id: string; };
+    "last_edited_by": { object: string; id: string; };
+    cover: { type: string; external: { url: string; } } | null;
+    icon: { type: string; external: { url: string; } } | null;
+    parent: { type: string; database_id: string; };
+    archived: boolean;
+    properties: { Desc: { id: string; }; Date: { id: string; }; Task: { id: string; } };
+    url: string;
+  }
+}
+
 const main = async () => {
   const currentTasks = await notion();
   console.log('Current Tasks:', currentTasks);
