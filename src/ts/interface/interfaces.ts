b65e320b387341648e10aac8e1a19308a1ee5673
Timothy <Rorie.TimothyJr@gmail.com>
2022-08-16 16:30:49 -0400

    Move interfaces to separate file

---
+++ src/ts/interface/interfaces.ts
@@ -0,0 +1,48 @@
+interface NotionTasksPayload {
+  [index: number]: {
+    object: string;
+    id: string;
+    created_time: string;
+    last_edited_time: string;
+    created_by: { object: string; id: string; };
+    last_edited_by: { object: string; id: string; };
+    cover?: { type: string; external: { url: string; } } | null;
+    icon?: { type: string; external: { url: string; } } | null;
+    parent: { type: string; database_id: string; };
+    archived: boolean;
+    properties: { Desc: { id: string; }; Date: { id: string; }; Task: { id: string; } };
+    url: string;
+    is_inline?: boolean;
+  }
+}
+
+interface PropertyItem {
+  object: string;
+  type: string;
+  id: string;
+  rich_text: {
+    type: string;
+    text: {};
+    annotations?: {};
+    plain_text: string;
+    href?: null
+  }
+}
+
+interface PromiseComplete {
+  notion(): Promise<
+    Array<{ pageId: string; task: string; description: string; dueDate: object | string }>
+  >;
+}
+
+interface PropertyValue {
+  pageId: string;
+  propertyId: string;
+}
+
+// interface PromisePayload {
+//   getPropertyValue({
+//     pageId,
+//     propertyId,
+//   }: PropertyValue): Promise<PropertyItemObject | Array<PropertyItemObject>>;
+// }
