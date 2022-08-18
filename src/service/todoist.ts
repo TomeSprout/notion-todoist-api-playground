f8f314cf8b101cd5c906f8a32e45bb479e2543fe
Timothy <Rorie.TimothyJr@gmail.com>
2022-08-18 13:39:56 -0400

    Add check for if Notion label pre-exists
    Remove unnecessary async.
    Collect IDs of created Notion Tasks.

--- src/service/todoist.ts
+++ src/service/todoist.ts
@@ -25,45 +25,48 @@ export const todoist = async () => {
   const todoist = new TodoistApi(process.env.TODOIST_TOKEN as string);
 
   const payload = await notion();
-  const parentTaskID: number = 6096455839;
-  const todoistPriorityList = {
-    high: 4,
-    med: 3,
-    low: 2,
-    none: 1,
-  }
+  const NotionTodoistTasks = [];
+  const parentTaskID: number = 6097394348;
+  const todoistPriority = { high: 4, med: 3, low: 2, none: 1 };
   let inboxProjectID: number = 0;
   let labelNotionID: number = 0;
 
-  await todoist.addLabel({ name: 'Notion', color: 49 })
-    .then((label) => labelNotionID = label.id)
-    .catch((error) => console.log(error));
+  await todoist.getLabels().then((labels) => {
+    labels.map(async (label) => {
+      label.name === "Notion"
+        ? (labelNotionID = label.id)
+        : (labelNotionID = 0);
+    });
+  });
 
-  payload.map(async task => {
+  if (labelNotionID === 0) {
+    await todoist.addLabel({ name: "Notion", color: 49 }).then((label) => {
+      labelNotionID = label.id;
+    });
+  }
+
+  payload.map(async (task) => {
     await todoist.addTask({
       content: task.title,
       description: task.description,
       dueString: "in 3 days",
       dueLang: "en",
-      priority: todoistPriorityList.low,
+      priority: todoistPriority.none,
       parentId: parentTaskID,
       labelIds: [labelNotionID],
-    })
+    });
   });
 
-  await todoist
-    .getProjects()
-    .then((projects) =>
-      projects.map(async (project: TodoistProject) => {
-        project.name === 'Inbox' && (inboxProjectID = project.id);        
-      })
-    )
-    .catch((error) => console.error());
-
-  await todoist
-    .getTasks(labelNotionID)
-    .then((tasks) => {
-      console.log(tasks.content);
+  await todoist.getProjects().then((projects) =>
+    projects.map((project: TodoistProject) => {
+      project.name === "Inbox" && (inboxProjectID = project.id);
     })
-  .catch(error => console.error());
+  );
+
+  await todoist.getTasks(parentTaskID).then((tasks) => {
+    tasks.map(
+      (task) =>
+        task.parentId === parentTaskID && NotionTodoistTasks.push(task.id)
+    );
+  });
 };
